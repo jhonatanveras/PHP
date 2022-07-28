51e06c439f754e8468cbcdf4a3b79fb257a95ff4
Jhonatan Veras <jveras@leroymerlin.com.br>
2022-07-28 17:26:26 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assests/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                            <a href="#">Exercício B</a>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                            <a href="#">Exercício B</a>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 03</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                            <a href="#">Exercício B</a>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 04</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                            <a href="#">Exercício B</a>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 05</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                            <a href="#">Exercício B</a>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 06</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                            <a href="#">Exercício B</a>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 07</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                            <a href="#">Exercício B</a>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 08</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                            <a href="#">Exercício B</a>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Módulo 09</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Exercício A</a>
+                            <a href="#">Exercício B</a>
+                            <a href="#">Exercício C</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & UDEMY © <?= date('Y');?>
+    </footer>
+</body>
+</html>
